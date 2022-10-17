0b1b8176c7526150ff3c4da5fa9435cdcdbdd610
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-10-17 18:52:11 +0900

    gallary works design break

--- js/parts.js
+++ js/parts.js
@@ -47,3 +47,24 @@ $(window).on('scroll', function(){
 $(window).on('load', function(){
 	ScrollTimelineAnime();// 線が伸びる関数を呼ぶ
 });
+
+
+//上部画像の設定
+$('.gallery').slick({
+	infinite: true, //スライドをループさせるかどうか。初期値はtrue。
+	fade: true, //フェードの有効化
+	arrows: true,//左右の矢印あり
+	prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+	nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+	asNavFor: '.choice-btn', //連動させるサムネイルのクラス名
+});
+
+//選択画像の設定
+$('.choice-btn').slick({
+	infinite: true, //スライドをループさせるかどうか。初期値はtrue。
+	slidesToShow: 4, //表示させるスライドの数
+	focusOnSelect: true, //フォーカスの有効化
+	prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+	nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+	asNavFor: '.gallery', //連動させるスライドショーのクラス名
+});
